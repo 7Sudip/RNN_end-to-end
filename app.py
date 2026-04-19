b1c34dd3c590c5db6c6d5004da3531c4ee71d055
7Sudip <sudipbohara82@gmail.com.com>
2026-04-19 21:38:55 +0545

    Add Model_Info.py file

--- app.py
+++ app.py
@@ -6,3 +6,5 @@ st.title("Movie Sentiment Analysis App")
 st.write("Welcome! Use the sidebar to navigate.")
 
 st.info("Go to Predict page to analyze sentiment.") 
+st.info("Go to Batch_Prediction to CSV File sentiment analyze") 
+st.info('Go to Model_info to look the Model information')
---
+++ pages/3_Model_Info.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Model Info")
+
+st.write("Model: Deep RNN")
+st.write("Max Length: 250")
+st.write("Vocabulary Size: 10000") 
